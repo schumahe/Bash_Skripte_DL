9b84ceef41921f3d5ca4db6f5c213f52fb4a908f
Heinke Schumacher <heinke.schumacher@sub.uni-hamburg.de>
2021-11-30 10:00:54 +0100

    Add bash script

---
+++ count_lines.sh
@@ -0,0 +1,5 @@
+ # Ein kleines Skript zum Zählen von Zeilen in tsv-Dateien
+ # 2021 - Heinke Schumacher <heinke.schumacher@sub.uni-hamburg.de>
+ echo "Count lines in CSV files"
+ wc -l *tsv
+ echo "Done! Have a nice day"
